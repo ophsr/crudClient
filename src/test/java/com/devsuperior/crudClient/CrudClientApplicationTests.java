7ba5aa072a0281c5e02235e22d5242cb71be40c8
Pedro <phsr2001@gmail.com>
2020-10-01 15:29:34 -0300

    First commit, Client Entity

---
+++ src/test/java/com/devsuperior/crudClient/CrudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.crudClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
